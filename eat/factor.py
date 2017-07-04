fd6b892a3dbf5bd02aedad1289a65f943737fa4c
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-07-04 09:21:48 -0400

    Roughly define the prototype

--- eat/factor.py
+++ eat/factor.py
@@ -1,5 +1,11 @@
-def factor():
+def factor(bb):
     """
     Factor out site-based delay and rate from baseline-based delay and rate
+
+    Args:
+        bb:    Baseline-based input data
+
+    Returns:
+        Site-based data being factored out
     """
-    pass
+    return None
