240cc97311ff87da6e9179a76e37aaf46ade7469
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-07-04 13:42:18 -0400

    Check if factor() works with numpy struct array and pandas dataframe

--- eat/tests/test_factor.py
+++ eat/tests/test_factor.py
@@ -1,7 +1,17 @@
+import pandas as pd
 from ..factor import *
 
 def test_factor():
     """
     Test if factor() successfully factors out site-based delay and rate
     """
-    pass
+    bb = [('a','b',0), ('b','c',0), ('c','d',0), ('d','e',0), ('e','a',0),
+          ('a','c',0), ('b','d',0), ('c','e',0), ('d','a',0), ('e','b',0)]
+
+    x  = np.array(bb, dtype=[('ref', 'U3'),('rem', 'U2'), ('val', 'f16')])
+    sb = factor(x)
+    assert sb is not None
+
+    y  = pd.DataFrame(x)
+    sb = factor(y)
+    assert sb is not None
