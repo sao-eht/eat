a84af679f3b8af1bb660fca6a513303905bf9a2e
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-07-04 10:31:27 -0400

    Improve documentation

--- eat/factor.py
+++ eat/factor.py
@@ -2,13 +2,33 @@ import numpy as np
 
 def factor(bb):
     """
-    Factor out site-based delay and rate from baseline-based delay and rate
+    Factor out site-based delay and rate from baseline-based slopes
+
+    The linear drift (slopes) of phase with frequency and time are
+    usually interpreted as delays and rates, and are removed from the
+    VLBI data in fringe fitting.  Let n be the number of sites.  There
+    are n (n-1) such slopes in total.  Using all of them, such as
+    currently done in HOPS, breaks the closure relationships in
+    general.
+
+    Global fringe solution takes into account the assumption that
+    delays and rates are site-based.  The main advantage is that it
+    preserves the closure relationships.  Even better, "one person's
+    noise is another person's signal", the remaining slopes after
+    global fringe fit actually contain information about the source
+    images.  Being able to factor out these site-based "noise" from
+    the baseline-based "data", therefore, is essential for developing
+    new high-order image reconstruction methods.
+
+    We implement here a very general apporach to factor out site-based
+    information from baseline-based information.
 
     Args:
         bb:    Baseline-based input data
 
     Returns:
         Site-based data being factored out
+
     """
     sites = np.unique(np.append(bb['ref'], bb['rem']))
     types = [('site', sites.dtype), ('value', 'f8')]
