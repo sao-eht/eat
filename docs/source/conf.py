4d063e4a02959cef899e9b55691eef8ae6e737e3
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-08-29 01:25:50 -0400

    Adjust "source/conf.py"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,10 +16,11 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath('../../eat'))
 
 
 # -- General configuration ------------------------------------------------
@@ -123,7 +124,7 @@ html_sidebars = {
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'EHTAnalysisToolkitdoc'
+htmlhelp_basename = 'eatdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -150,7 +151,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'EHTAnalysisToolkit.tex', 'EHT Analysis Toolkit Documentation',
+    (master_doc, 'eat.tex', 'EHT Analysis Toolkit Documentation',
      'Lindy Blackburn', 'manual'),
 ]
 
@@ -160,7 +161,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'ehtanalysistoolkit', 'EHT Analysis Toolkit Documentation',
+    (master_doc, 'eat', 'EHT Analysis Toolkit Documentation',
      [author], 1)
 ]
 
@@ -171,8 +172,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'EHTAnalysisToolkit', 'EHT Analysis Toolkit Documentation',
-     author, 'EHTAnalysisToolkit', 'One line description of project.',
+    (master_doc, 'eat', 'EHT Analysis Toolkit Documentation',
+     author, 'eat', 'One line description of project.',
      'Miscellaneous'),
 ]
 
