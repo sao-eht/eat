0ecb75225781f201b5f600ee01a8f3fc6a3eeb7a
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-09-28 03:16:55 -0400

    Allow site selection

--- eat/eht.py
+++ eat/eht.py
@@ -5,9 +5,15 @@ class eht:
     """
     A class describing the EHT telescope array
     """
-    def __init__(self):
+    def __init__(self, sites):
         modpath  = __name__.split(".")[0]
         fullname = pr.resource_filename(modpath, "data/sites.csv")
         with open(fullname, "r") as handle:
             reader = csv.DictReader(handle)
-            self.array = {dict['site']: dict for dict in reader}
+            if sites is None:
+                self.array = {d['site']: d
+                              for d in reader}
+            else:
+                self.array = {d['site']: d
+                              for d in reader
+                              if d['site'] in sites}
