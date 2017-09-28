4fca4db2af7b7ebc25c4836038dd13a26bc295cd
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-09-28 04:28:10 -0400

    Untested implementation of field rotation angle from mockservation

--- eat/site.py
+++ eat/site.py
@@ -1,6 +1,14 @@
+import numpy as np
+
 from astropy import units as u
 from astropy.coordinates import EarthLocation
 from astropy.coordinates import Angle
+from astropy.coordinates import AltAz
+
+def par_angle(hra, dec, lat):
+    y = np.sin(hra) * np.cos(lat)
+    x = np.sin(lat) * np.cos(dec) - np.cos(lat) * np.sin(dec) * np.cos(hra)
+    return np.arctan2(y, x)
 
 class Site:
     """
@@ -11,3 +19,8 @@ class Site:
         self.coord = EarthLocation(d['x_m'], d['y_m'], d['z_m'], u.m)
         self.cnst  = Angle(d['fra_offset'], u.degree)
         self.sign  = d['fra_elev_sign']
+
+    def fra(self, time, src_coord):
+        aa = src_coord.transform_to(AltAz(obstime=time, location=self.coord))
+        pa = par_angle(aa.az, src_coord.dec, self.coord.lat)
+        return pa + self.cnst + self.sign * aa.alt
