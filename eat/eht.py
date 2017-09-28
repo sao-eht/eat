7c5b7745f1475efdba26298424adb89b150542be
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-09-28 03:30:59 -0400

    Use the 2017 EHT array as the default

--- eat/eht.py
+++ eat/eht.py
@@ -5,15 +5,12 @@ class eht:
     """
     A class describing the EHT telescope array
     """
-    def __init__(self, sites):
+    def __init__(self, sites="AJLPRSXZ"):
         modpath  = __name__.split(".")[0]
         fullname = pr.resource_filename(modpath, "data/sites.csv")
         with open(fullname, "r") as handle:
             reader = csv.DictReader(handle)
-            if sites is None:
-                self.array = {d['site']: d
-                              for d in reader}
-            elif isinstance(sites, str):
+            if isinstance(sites, str):
                 self.array = {d['site']: d
                               for d in reader
                               if  d['site_id'] is not ''
