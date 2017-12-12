6545bbe28e15af28c6c0e18455c86d455bb71836
Lindy Blackburn <lindylam@gmail.com>
2017-12-12 09:51:38 -0500

    readme markdown

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from codecs     import open
 from os         import path
 
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, "README"), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
