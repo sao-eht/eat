0ab62a42b17a7a40670281b7c7c81171be71c551
lindylam@gmail.com <lindylam@gmail.com>
2018-03-04 10:28:00 +0000

    support polconvert fixes in alist files

--- eat/io/util.py
+++ eat/io/util.py
@@ -167,6 +167,31 @@ def noauto(df):
     auto = df.baseline.str[0] == df.baseline.str[1]
     return df[~auto].copy()
 
+# a number of polconvert fixes based on rootcode (correlation proc time)
+# optionally undo fix
+def fix(df):
+    # sqrt2 fix er2lo:('zplptp', 'zrmvon') er2hi:('zplscn', 'zrmvoi')
+    idx = (df.baseline.str.count('A') == 1) & (df.root_id > 'zpaaaa') & (df.root_id < 'zrzzzz')
+    df.loc[idx,'snr'] /= np.sqrt(2.0)
+    df.loc[idx,'amp'] /= np.sqrt(2.0)
+    # swap polarization fix er3lo:('zxuerf', 'zyjmiy') er3hi:('zymrse', 'zztobd')
+    idx1 = df.baseline.str.contains('A') & (df.polarization == 'LR') & (df.root_id > 'zxaaaa') & (df.root_id < 'zzzzzz')
+    idx2 = df.baseline.str.contains('A') & (df.polarization == 'RL') & (df.root_id > 'zxaaaa') & (df.root_id < 'zzzzzz')
+    df.loc[idx1,'polarization'] = 'RL'
+    df.loc[idx2,'polarization'] = 'LR'
+    
+def undofix(df):        
+# a number of polconvert fixes based on rootcode (correlation proc time)
+    # sqrt2 fix er2lo:('zplptp', 'zrmvon') er2hi:('zplscn', 'zrmvoi')
+    idx = (df.baseline.str.count('A') == 1) & (df.root_id > 'zpaaaa') & (df.root_id < 'zrzzzz')
+    df.loc[idx,'snr'] *= np.sqrt(2.0)
+    df.loc[idx,'amp'] *= np.sqrt(2.0)
+    # swap polarization fix er3lo:('zxuerf', 'zyjmiy') er3hi:('zymrse', 'zztobd')
+    idx1 = df.baseline.str.contains('A') & (df.polarization == 'LR') & (df.root_id > 'zxaaaa') & (df.root_id < 'zzzzzz')
+    idx2 = df.baseline.str.contains('A') & (df.polarization == 'RL') & (df.root_id > 'zxaaaa') & (df.root_id < 'zzzzzz')
+    df.loc[idx1,'polarization'] = 'RL'
+    df.loc[idx2,'polarization'] = 'LR'
+
 def uvdict(filename):
     """take calibration output data frame, and make UV dictionary lookup table"""
     from . import hops
