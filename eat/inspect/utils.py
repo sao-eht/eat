71dffad9ade5eb366be93ec186db7eacbe6eb15c
Maciek Wielgus <maciek.wielgus@gmail.com>
2018-07-18 20:18:25 -0400

    debugging std_mean

--- eat/inspect/utils.py
+++ eat/inspect/utils.py
@@ -948,6 +948,7 @@ def coh_avg_vis(frame,tavg='scan',columns_out0=[],phase_type='resid_phas'):
     #frame_avg['sigma']=frame_avg['amp']/frame_avg['snr']
 
     frame_avg = frame_avg.reset_index()
+    columns_out0 = list(set(columns_out0))
     columns_out = columns_out0+list(grouping0)+list(set(['source','datetime','amp','phase','snr','sigma','number'])&set(frame_avg.columns))
     print('columns_out0:', columns_out0)
     print('grouping0:', list(grouping0))
