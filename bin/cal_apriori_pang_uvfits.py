5a67c396d9aad39a80ecfaab1a6795cdadd902cb
Maciek Wielgus <maciek.wielgus@gmail.com>
2019-03-07 22:15:06 -0500

    to process gmva

--- bin/cal_apriori_pang_uvfits.py
+++ bin/cal_apriori_pang_uvfits.py
@@ -48,7 +48,9 @@ station_dic = {'ALMA':'AA', 'A':'AA','AA':'AA',
             'JCMT':'JC','J':'JC','JC':'JC',
             'SMAP':'SM','S':'SM','SMAR':'SM','SMA':'SM','SM':'SM',
             'SMAR':'SR','R':'SR','SMR':'SR','SR':'SR',
-            'GBT': 'G'
+            'GBT': 'GB','FD': 'FD','PT':'PT','LA':'LA', 
+            'KP':'KP', 'MK':'MK', 'BR':'BR', 'NL':'NL',
+            'OV':'OV','YS':'YS','EB':'EB'
             }
 
 station_frot = {'PV':(1,-1,0),'AZ':(1,1,0),'SM':(1,-1,np.pi/4.),'LM': (1,-1,0),'AA':(1,0,0),'SP':(1,0,0),'AP':(1,1,0),'JC':(1,0,0),'SR':(1,-1,np.pi/4.),'GB':(1,0,0)}
