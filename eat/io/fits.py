5e81e18d4007516a64f7d3b5a34d7f914bbba1a9
Lindy Blackburn <lindy@SI385756.local>
2020-10-19 13:42:28 -0400

    file open modes and proper close

--- eat/io/fits.py
+++ eat/io/fits.py
@@ -2,23 +2,30 @@
 # standard_library.install_aliases()
 # from builtins import next
 # from builtins import object
+try:
+    from StringIO import StringIO
+except:
+    from io import StringIO
 from astropy.io import fits
+from argparse import Namespace as ns
 
 # some easier interactive fits interface
 class fitshdulist(object):
 
     def __init__(self, hdulist):
         self.hdulist = hdulist
+        self.extensions = ns()
 
     def __dir__(self):
-        return [a.name for a in self.hdulist]
+        return [a.name for a in self.hdulist] + (['extensions'] if (len(self.extensions.__dict__) > 0) else [])
 
     def __repr__(self):
         # from io import StringIO
         # at some point (?) io.StringIO stopped accepting byte strings, need revisit in 3.x
-        from StringIO import StringIO
         out = StringIO()
         self.hdulist.info(output=out)
+        for (name, val) in self.extensions.__dict__.items():
+            out.write('extensions.%s - %s' % (name, repr(val)))
         return out.getvalue()
 
     def __getitem__(self, item):
@@ -32,6 +39,9 @@ class fitshdulist(object):
             # https://github.com/astropy/astropy/issues/5585
             # return fitshdu(next((a for a in reversed(self.hdulist) if attr.upper() in a.name)))
             return fitshdu(next((a for a in self.hdulist[::-1] if attr.upper() in a.name)))
+
+    def close(self):
+        hdulist.close()
 
 # some easier interactive fits interface
 class fitshdu(object):
@@ -61,7 +71,8 @@ class fitshdu(object):
         elif hasattr(self.hdu, 'columns'):
             return next(self.hdu.data[a.name] for a in self.hdu.columns if attr.upper() in a.name)
 
-def open(filename):
+# use mode='update' if we need to change file
+def open(filename, mode='readonly'):
     """open fits file into convenient contained class for interactive work
 
     Args:
@@ -72,5 +83,20 @@ def open(filename):
     """
     # mode='denywrite' here to avoid annoying memmap preallocation problem with mmap.ACCESS_COPY on large files
     # https://github.com/astropy/astropy/issues/1380
-    hdulist = fits.open(filename, mode='denywrite')
+    # hdulist = fits.open(filename, mode='denywrite')
+    # update: perhaps this is fixed as of 2018
+    hdulist = fits.open(filename, mode=mode)
     return fitshdulist(hdulist)
+
+class uvfits(object):
+
+    def __init__(self, hdulist):
+        if hasattr(hdulist, 'extensions'):
+            self.hdulist = hdulist.hdulist
+            hdulist.extensions.uvfits = self
+
+    def __repr__(self):
+        out = StringIO()
+        out.write('[%s]\n' % self.hdulist.filename())
+        return out.getvalue()
+
