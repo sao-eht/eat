0165e906940c3e6515046715f056c95396de0ead
Lindy Blackburn <lindylam@gmail.com>
2024-10-25 17:42:01 -0400

    recover mk4.py and difx.py from previously deleted

---
+++ eat/io/difx.py
@@ -0,0 +1,425 @@
+from ctypes import *
+
+try:
+    difxio = cdll.LoadLibrary('/home/lindy/difx/lib/libdifxio.so')
+except:
+    # TOOD: avoid this ugly code
+    import sys
+    if 'sphinx' not in sys.modules:
+        raise
+
+class DifxAntennaFlag(Structure):
+    _fields_ = [
+    ('mjd1', c_double),
+    ('mjd2', c_double),
+    ('antennaId', c_int),
+]
+
+class DifxJob(Structure):
+    _fields_ = [
+    ('difxVersion', c_char * 64),
+    ('difxLabel', c_char * 64),
+    ('jobStart', c_double),
+    ('jobStop', c_double),
+    ('mjdStart', c_double),
+    ('duration', c_double),
+    ('jobId', c_int),
+    ('subjobId', c_int),
+    ('subarrayId', c_int),
+    ('obsCode', c_char * 8),
+    ('obsSession', c_char * 8),
+    ('taperFunction', c_int),
+    ('calcServer', c_char * 32),
+    ('calcVersion', c_int),
+    ('calcProgram', c_int),
+    ('activeDatastreams', c_int),
+    ('activeBaselines', c_int),
+    ('polyOrder', c_int),
+    ('polyInterval', c_int),
+    ('aberCorr', c_int),
+    ('dutyCycle', c_double),
+    ('nFlag', c_int),
+    ('flag', POINTER(DifxAntennaFlag)),
+    ('vexFile', c_char * 256),
+    ('inputFile', c_char * 256),
+    ('calcFile', c_char * 256),
+    ('imFile', c_char * 256),
+    ('flagFile', c_char * 256),
+    ('threadsFile', c_char * 256),
+    ('outputFile', c_char * 256),
+    ('jobIdRemap', POINTER(c_int)),
+    ('freqIdRemap', POINTER(c_int)),
+    ('antennaIdRemap', POINTER(c_int)),
+    ('datastreamIdRemap', POINTER(c_int)),
+    ('baselineIdRemap', POINTER(c_int)),
+    ('pulsarIdRemap', POINTER(c_int)),
+    ('configIdRemap', POINTER(c_int)),
+    ('sourceIdRemap', POINTER(c_int)),
+    ('spacecraftIdRemap', POINTER(c_int)),
+]
+
+class DifxIF(Structure):
+    _fields_ = [
+    ('freq', c_double),
+    ('bw', c_double),
+    ('sideband', c_char),
+    ('nPol', c_int),
+    ('pol', c_char * 2),
+    ('rxName', c_char * 8),
+]
+
+class DifxConfig(Structure):
+    _fields_ = [
+    ('name', c_char * 32),
+    ('tInt', c_double),
+    ('subintNS', c_int),
+    ('guardNS', c_int),
+    ('fringeRotOrder', c_int),
+    ('strideLength', c_int),
+    ('xmacLength', c_int),
+    ('numBufferedFFTs', c_int),
+    ('pulsarId', c_int),
+    ('phasedArrayId', c_int),
+    ('nPol', c_int),
+    ('pol', c_char * 2),
+    ('polMask', c_int),
+    ('doPolar', c_int),
+    ('doAutoCorr', c_int),
+    ('quantBits', c_int),
+    ('nAntenna', c_int),
+    ('nDatastream', c_int),
+    ('nBaseline', c_int),
+    ('datastreamId', POINTER(c_int)),
+    ('baselineId', POINTER(c_int)),
+    ('nIF', c_int),
+    ('IF', POINTER(DifxIF)),
+    ('fitsFreqId', c_int),
+    ('freqId2IF', POINTER(c_int)),
+    ('freqIdUsed', POINTER(c_int)),
+    ('ant2dsId', POINTER(c_int)),
+]
+
+class DifxStringArray(Structure):
+    _fields_ = [
+    ('str', POINTER(POINTER(c_char))),
+    ('n', c_int),
+    ('nAlloc', c_int),
+]
+
+class DifxRule(Structure):
+    _fileds_ = [
+    ('sourceName', DifxStringArray),
+    ('scanId', DifxStringArray),
+    ('calCode', c_char * 4),
+    ('qual', c_int),
+    ('mjdStart', c_double),
+    ('mjdStop', c_double),
+    ('configName', c_char * 32),
+]
+
+class DifxFreq(Structure):
+    _fields_ = [
+    ('freq', c_double),
+    ('bw', c_double),
+    ('sideband', c_char),
+    ('nChan', c_int),
+    ('specAvg', c_int),
+    ('overSamp', c_int),
+    ('decimation', c_int),
+    ('nTone', c_int),
+    ('tone', POINTER(c_int)),
+    ('rxName', c_char * 8),
+]
+
+class DifxAntenna(Structure):
+    _fields_ = [
+    ('name', c_char * 32),
+    ('origId', c_int),
+    ('clockrefmjd', c_double),
+    ('clockorder', c_int),
+    ('clockcoeff', c_double * (5 + 1)),
+    ('mount', c_int),
+    ('siteType', c_int),
+    ('offset', c_double * 3),
+    ('X', c_double),
+    ('Y', c_double),
+    ('Z', c_double),
+    ('dX', c_double),
+    ('dY', c_double),
+    ('dZ', c_double),
+    ('spacecraftId', c_int),
+    ('shelf', c_char * 8),
+]
+
+class DifxPolyModel(Structure):
+    _fields_ = [
+    ('mjd', c_int),
+    ('sec', c_int),
+    ('order', c_int),
+    ('validDuration', c_int),
+    ('delay', c_double * (5 + 1)),
+    ('dry', c_double * (5 + 1)),
+    ('wet', c_double * (5 + 1)),
+    ('az', c_double * (5 + 1)),
+    ('elcorr', c_double * (5 + 1)),
+    ('elgeom', c_double * (5 + 1)),
+    ('parangle', c_double * (5 + 1)),
+    ('u', c_double * (5 + 1)),
+    ('v', c_double * (5 + 1)),
+    ('w', c_double * (5 + 1)),
+]
+
+class DifxPolyModelExtension(Structure):
+    _fields_ = [
+    ('delta', c_double),
+    ('dDelay_dl', c_double * (5 + 1)),
+    ('dDelay_dm', c_double * (5 + 1)),
+    ('d2Delay_dldl', c_double * (5 + 1)),
+    ('d2Delay_dldm', c_double * (5 + 1)),
+    ('d2Delay_dmdm', c_double * (5 + 1)),
+]
+
+class DifxPolyModelLMExtension(Structure):
+    _fields_ = [
+    ('delta', c_double),
+    ('dDelay_dl', c_double * (5 + 1)),
+    ('dDelay_dm', c_double * (5 + 1)),
+    ('d2Delay_dldl', c_double * (5 + 1)),
+    ('d2Delay_dldm', c_double * (5 + 1)),
+    ('d2Delay_dmdm', c_double * (5 + 1)),
+]
+
+class DifxPolyModelXYZExtension(Structure):
+    _fields_ = [
+    ('delta', c_double),
+    ('dDelay_dX', c_double * (5 + 1)),
+    ('dDelay_dY', c_double * (5 + 1)),
+    ('dDelay_dZ', c_double * (5 + 1)),
+    ('d2Delay_dXdX', c_double * (5 + 1)),
+    ('d2Delay_dXdY', c_double * (5 + 1)),
+    ('d2Delay_dXdZ', c_double * (5 + 1)),
+    ('d2Delay_dYdY', c_double * (5 + 1)),
+    ('d2Delay_dYdZ', c_double * (5 + 1)),
+    ('d2Delay_dZdZ', c_double * (5 + 1)),
+]
+
+class DifxScan(Structure):
+    _fields_ = [
+    ('mjdStart', c_double),
+    ('mjdEnd', c_double),
+    ('startSeconds', c_int),
+    ('durSeconds', c_int),
+    ('identifier', c_char * 32),
+    ('obsModeName', c_char * 32),
+    ('maxNSBetweenUVShifts', c_int),
+    ('maxNSBetweenACAvg', c_int),
+    ('pointingCentreSrc', c_int),
+    ('nPhaseCentres', c_int),
+    ('phsCentreSrcs', c_int * 1000),
+    ('orgjobPhsCentreSrcs', c_int * 1000),
+    ('jobId', c_int),
+    ('configId', c_int),
+    ('nAntenna', c_int),
+    ('nPoly', c_int),
+    ('im', POINTER(POINTER(POINTER(DifxPolyModel)))),
+    ('imLM', POINTER(POINTER(POINTER(DifxPolyModelLMExtension)))),
+    ('imXYZ', POINTER(POINTER(POINTER(DifxPolyModelXYZExtension)))),
+]
+
+class DifxSource(Structure):
+    _fields_ = [
+    ('ra', c_double),
+    ('dec', c_double),
+    ('name', c_char * 32),
+    ('calCode', c_char * 4),
+    ('qual', c_int),
+    ('spacecraftId', c_int),
+    ('numFitsSourceIds', c_int),
+    ('fitsSourceIds', POINTER(c_int)),
+    ('pmRA', c_double),
+    ('pmDec', c_double),
+    ('parallax', c_double),
+    ('pmEpoch', c_double),
+]
+
+class DifxEOP(Structure):
+    _fields_ = [
+    ('mjd', c_int),
+    ('tai_utc', c_int),
+    ('ut1_utc', c_double),
+    ('xPole', c_double),
+    ('yPole', c_double),
+]
+
+class DifxDatastream(Structure):
+    _fields_ = [
+    ('antennaId', c_int),
+    ('tSys', c_float),
+    ('dataFormat', c_char * 128),
+    ('dataSampling', c_int),
+    ('nFile', c_int),
+    ('file', POINTER(POINTER(c_char))),
+    ('networkPort', c_char * 12),
+    ('windowSize', c_int),
+    ('quantBits', c_int),
+    ('dataFrameSize', c_int),
+    ('dataSource', c_int),
+    ('phaseCalIntervalMHz', c_int),
+    ('tcalFrequency', c_int),
+    ('nRecTone', c_int),
+    ('recToneFreq', POINTER(c_int)),
+    ('recToneOut', POINTER(c_int)),
+    ('clockOffset', POINTER(c_double)),
+    ('clockOffsetDelta', POINTER(c_double)),
+    ('phaseOffset', POINTER(c_double)),
+    ('freqOffset', POINTER(c_double)),
+    ('nRecFreq', c_int),
+    ('nRecBand', c_int),
+    ('nRecPol', POINTER(c_int)),
+    ('recFreqId', POINTER(c_int)),
+    ('recBandFreqId', POINTER(c_int)),
+    ('recBandPolName', POINTER(c_char)),
+    ('nZoomFreq', c_int),
+    ('nZoomBand', c_int),
+    ('nZoomPol', POINTER(c_int)),
+    ('zoomFreqId', POINTER(c_int)),
+    ('zoomBandFreqId', POINTER(c_int)),
+    ('zoomBandPolName', POINTER(c_char)),
+]
+
+class DifxBaseline(Structure):
+    _fields_ = [
+    ('dsA', c_int),
+    ('dsB', c_int),
+    ('nFreq', c_int),
+    ('nPolProd', POINTER(c_int)),
+    ('bandA', POINTER(POINTER(c_int))),
+    ('bandB', POINTER(POINTER(c_int))),
+]
+
+class sixVector(Structure):
+    _fields_ = [
+    ('mjd', c_int),
+    ('fracDay', c_double),
+    ('X', c_longdouble),
+    ('Y', c_longdouble),
+    ('Z', c_longdouble),
+    ('dX', c_longdouble),
+    ('dY', c_longdouble),
+    ('dZ', c_longdouble),
+]
+
+class RadioastronTimeFrameOffset(Structure):
+    _fields_ = [
+    ('Delta_t', c_double),
+    ('dtdtau', c_double),
+]
+
+class RadioastronAxisVectors(Structure):
+    _fields_ = [
+    ('X', c_double * 3),
+    ('Y', c_double * 3),
+    ('Z', c_double * 3),
+]
+
+class DifxSpacecraft(Structure):
+    _fields_ = [
+    ('name', c_char * 32),
+    ('nPoint', c_int),
+    ('pos', POINTER(sixVector)),
+    ('timeFrameOffset', POINTER(RadioastronTimeFrameOffset)),
+    ('axisVectors', POINTER(RadioastronAxisVectors)),
+    ('frame', c_char * 32),
+]
+
+class DifxPolyco(Structure):
+    _fields_ = [
+    ('fileName', c_char * 256),
+    ('dm', c_double),
+    ('refFreq', c_double),
+    ('mjd', c_double),
+    ('nCoef', c_int),
+    ('nBlk', c_int),
+    ('p0', c_double),
+    ('f0', c_double),
+    ('coef', POINTER(c_double)),
+]
+
+class DifxPulsar(Structure):
+    _fields_ = [
+    ('fileName', c_char * 256),
+    ('nPolyco', c_int),
+    ('polyco', POINTER(DifxPolyco)),
+    ('nBin', c_int),
+    ('binEnd', POINTER(c_double)),
+    ('binWeight', POINTER(c_double)),
+    ('scrunch', c_int),
+]
+
+class DifxPhasedArray(Structure):
+    _fields_ = [
+    ('fileName', c_char * 256),
+    ('outputType', c_int),
+    ('outputFormat', c_int),
+    ('accTime', c_double),
+    ('complexOutput', c_int),
+    ('quantBits', c_int),
+]
+
+class DifxInput(Structure):
+    _fields_ = [
+    ('fracSecondStartTime', c_int),
+    ('mjdStart', c_double),
+    ('mjdStop', c_double),
+    ('refFreq', c_double),
+    ('startChan', c_int),
+    ('specAvg', c_int),
+    ('nInChan', c_int),
+    ('nOutChan', c_int),
+    ('visBufferLength', c_int),
+    ('nIF', c_int),
+    ('nPol', c_int),
+    ('doPolar', c_int),
+    ('nPolar', c_int),
+    ('chanBW', c_double),
+    ('quantBits', c_int),
+    ('polPair', c_char * 4),
+    ('dataBufferFactor', c_int),
+    ('nDataSegments', c_int),
+    ('outputFormat', c_int),
+    ('eopMergeMode', c_int),
+    ('nCore', c_int),
+    ('nThread', POINTER(c_int)),
+    ('nAntenna', c_int),
+    ('nConfig', c_int),
+    ('nRule', c_int),
+    ('nFreq', c_int),
+    ('nScan', c_int),
+    ('nSource', c_int),
+    ('nEOP', c_int),
+    ('nFlag', c_int),
+    ('nDatastream', c_int),
+    ('nBaseline', c_int),
+    ('nSpacecraft', c_int),
+    ('nPulsar', c_int),
+    ('nPhasedArray', c_int),
+    ('nJob', c_int),
+    ('job', POINTER(DifxJob)),
+    ('config', POINTER(DifxConfig)),
+    ('rule', POINTER(DifxRule)),
+    ('freq', POINTER(DifxFreq)),
+    ('antenna', POINTER(DifxAntenna)),
+    ('scan', POINTER(DifxScan)),
+    ('source', POINTER(DifxSource)),
+    ('eop', POINTER(DifxEOP)),
+    ('datastream', POINTER(DifxDatastream)),
+    ('baseline', POINTER(DifxBaseline)),
+    ('spacecraft', POINTER(DifxSpacecraft)),
+    ('pulsar', POINTER(DifxPulsar)),
+    ('phasedarray', POINTER(DifxPhasedArray)),
+]
+
+def loadDifxInput(filePrefix = '/media/lindy/DATA/smm-pc/d21us_clean/h_1609'):
+    difxio.loadDifxInput.restype = POINTER(DifxInput)
+    a = difxio.loadDifxInput(c_char_p(filePrefix))
+    return a.contents
---
+++ eat/io/mk4.py
@@ -0,0 +1,373 @@
+from builtins import object
+from ctypes import *
+
+try:
+    mk4io = cdll.LoadLibrary('/home/lindy/hops-3.10/sub/mk4.so')
+except:
+    # TOOD: avoid this ugly code
+    import sys
+    if 'sphinx' not in sys.modules:
+        raise
+
+# int
+# read_mk4corel (char *filename,
+#                struct mk4_corel *corel)
+
+class type_000(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('unused1', c_char * 3),
+    ('date', c_char * 16),
+    ('name', c_char * 40),
+]
+
+class type_100(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('unused1', c_char * 3),
+    ('date', c_char * 16),
+    ('name', c_char * 40),
+]
+
+class date(Structure):
+    _fields_ = [
+    ('year', c_short),
+    ('day', c_short),
+    ('hour', c_short),
+    ('minute', c_short),
+    ('second', c_float),
+]
+
+class type_100(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('unused1', c_char * 3),
+    ('procdate', date),
+    ('baseline', c_char * 2),
+    ('rootname', c_char * 34),
+    ('qcode', c_char * 2),
+    ('unused2', c_char * 6),
+    ('pct_done', c_float),
+    ('start', date),
+    ('stop', date),
+    ('ndrec', c_int),
+    ('nindex', c_int),
+    ('nlags', c_short),
+    ('nblocks', c_short),
+]
+
+class type_101(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('status', c_char),
+    ('nblocks', c_short),
+    ('index', c_short),
+    ('primary', c_short),
+    ('ref_chan_id', c_char * 8),
+    ('rem_chan_id', c_char * 8),
+    ('corr_board', c_short),
+    ('corr_slot', c_short),
+    ('ref_chan', c_short),
+    ('rem_chan', c_short),
+    ('post_mortem', c_int),
+    ('blocks', c_int),
+]
+
+class flag_wgt(Union):
+    _fields_ = [
+    ('flag', c_int),
+    ('weight', c_float),
+]
+
+class counts_per_lag(Structure):
+    _fields_ = [
+    ('coscor', c_int),
+    ('cosbits', c_int),
+    ('sincor', c_int),
+    ('sinbits', c_int),
+]
+
+class lag_tag(Structure):
+    _fields_ = [
+    ('coscor', c_int),
+    ('sincor', c_int),
+]
+
+class counts_global(Structure):
+    _fields_ = [
+    ('cosbits', c_int),
+    ('sinbits', c_int),
+    ('lags', lag_tag),
+]
+
+class auto_per_lag(Structure):
+    _fields_ = [
+    ('coscor', c_int),
+    ('cosbits', c_int),
+]
+
+class auto_global(Structure):
+    _fields_ = [
+    ('cosbits', c_int),
+    ('unused', c_char * 4),
+    ('coscor', c_int),
+]
+
+class spectral(Structure):
+    _fields_ = [
+    ('re', c_float),
+    ('im', c_float),
+]
+
+class lag_data(Union):
+    _fields_ = [
+    ('cpl', counts_per_lag),
+    ('cg', counts_global),
+    ('apl', auto_per_lag),
+    ('ag', auto_global),
+    ('spec', spectral),
+]
+
+class type_120(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('type', c_char),
+    ('nlags', c_short),
+    ('baseline', c_char * 2),
+    ('rootcode', c_char * 6),
+    ('index', c_int),
+    ('ap', c_int),
+    ('fw', flag_wgt),
+    ('status', c_int),
+    ('fr_delay', c_int),
+    ('delay_rate', c_int),
+    ('ld', lag_data),
+]
+
+class index_tag(Structure):
+    _fields_ = [
+    ('t101', POINTER(type_101)),   # a single t101 record
+    ('ap_space', c_int),
+    ('t120', POINTER(POINTER(type_120))),
+]
+
+class mk4_corel(Structure):
+    _fields_ = [
+    ('allocated', POINTER(None) * (8192 + 4)), # pointers to allocated blocks for memory management
+    ('nalloc', c_int),                         # the number of such pointers
+    ('file_image', POINTER(c_char)),
+    ('id', POINTER(type_000)),                 # single record
+    ('t100', POINTER(type_100)),               # single record
+    ('index_space', c_int),                    # length of index array
+    ('index', POINTER(index_tag)),
+]
+
+def loadmk4(filePrefix = '/media/lindy/DATA/dewi/3424/080-0500_LOW/FD..wxfrmh'):
+    # difxio.loadDifxInput.restype = POINTER(DifxInput)
+    mk4p = mk4_corel()
+    mk4io.read_mk4corel(c_char_p(filePrefix), byref(mk4p))
+    return mk4p
+
+class type_300(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('unused1', c_char * 2),
+    ('SU_number', c_ubyte),
+    ('id', c_char),
+    ('intl_id', c_char * 2),
+    ('name', c_char * 32),
+    ('unused2', c_char),
+    ('model_start', date),
+    ('model_interval', c_float),
+    ('nsplines', c_short),
+]
+
+class type_301(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('unused1', c_char * 3),
+    ('interval', c_short),
+    ('chan_id', c_char * 32),
+    ('unused2', c_char * 6),
+    ('delay_spline', c_double * 6),
+]
+
+class type_302(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('unused1', c_char * 3),
+    ('interval', c_short),
+    ('chan_id', c_char * 32),
+    ('unused2', c_char * 6),
+    ('phase_spline', c_double * 6),
+]
+
+class type_303(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('unused1', c_char * 3),
+    ('interval', c_short),
+    ('chan_id', c_char * 32),
+    ('unused2', c_char * 6),
+    ('azimuth', c_double * 6),
+    ('elevation', c_double * 6),
+    ('parallactic_angle', c_double * 6),
+    ('u', c_double * 6),
+    ('v', c_double * 6),
+    ('w', c_double * 6),
+]
+
+class type_304_anon(Structure):
+    _fields_ = [
+    ('error_count', c_int),
+    ('frames', c_int),
+    ('bad_frames', c_int),
+    ('slip_sync', c_int),
+    ('missing_sync', c_int),
+    ('crc_error', c_int),
+]
+
+class type_304(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('unused1', c_char * 3),
+    ('time', date),
+    ('duration', c_float),
+    ('trackstats', type_304_anon * 64),
+]
+
+class type_305(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('unused1', c_char * 3),
+]
+
+class type_306_anon(Structure):
+    _fields_ = [
+    ('chan_id', c_char * 32),
+    ('bigpos', c_int),
+    ('pos', c_int),
+    ('neg', c_int),
+    ('bigneg', c_int),
+]
+
+
+class type_306(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('unused1', c_char * 3),
+    ('time', date),
+    ('duration', c_float),
+    ('stcount', type_306_anon * 16),
+]
+
+class ChanCount(Structure):
+    _fields_ = [
+    ('count', c_uint32 * 8),
+    ('val_count', c_uint32),
+]
+
+class type_307(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('unused1', c_char * 3),
+    ('su', c_int),
+    ('unused2', c_char * 4),
+    ('tot', c_double),
+    ('rot', c_double),
+    ('accum_period', c_double),
+    ('frame_count', c_uint32),
+    ('counts', ChanCount * 16),
+    ('unused3', c_char * 4),
+]
+
+# /home/lindy/hops-3.10/include/type_308.h: 17
+class type_308_anon(Structure):
+    _fields_ = [
+    ('chan_id', c_char * 8),
+    ('frequency', c_float),
+    ('real', c_float),
+    ('imaginary', c_float),
+]
+
+class type_308(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('unused1', c_char * 3),
+    ('time', date),
+    ('duration', c_float),
+    ('pcal', type_308_anon * 32),
+]
+
+class ch1_tag(Structure):
+    _fields_ = [
+    ('chan_name', c_char * 8),
+    ('freq', c_double),
+    ('acc', (c_uint32 * 2) * 64),
+]
+
+class type_309(Structure):
+    _fields_ = [
+    ('record_id', c_char * 3),
+    ('version_no', c_char * 2),
+    ('unused1', c_char * 3),
+    ('su', c_int),
+    ('ntones', c_int),
+    ('rot', c_double),
+    ('acc_period', c_double),
+    ('chan', ch1_tag * 16),
+]
+
+class mk4_sdata_anon(Structure):
+    _fields_ = [
+    ('chan_id', c_char * 32),
+    ('t301', POINTER(type_301) * 64),
+    ('t302', POINTER(type_302) * 64),
+    ('t303', POINTER(type_303) * 64),
+]
+
+# /home/lindy/hops-3.10/include/mk4_data.h: 111
+class mk4_sdata(Structure):
+    _fields_ = [
+    ('allocated', POINTER(None) * ((((2 * 64) * 64) + (7 * 3600)) + 3)),
+    ('nalloc', c_int),
+    ('file_image', POINTER(c_char_p)),
+    ('id', POINTER(type_000)),
+    ('t300', POINTER(type_300)),
+    ('model', mk4_sdata_anon * 64),
+    ('n304', c_int),
+    ('n305', c_int),
+    ('n306', c_int),
+    ('n307', c_int),
+    ('n308', c_int),
+    ('n309', c_int),
+    ('t304', POINTER(type_304) * 3600),
+    ('t305', POINTER(type_305) * 3600),
+    ('t306', POINTER(type_306) * 3600),
+    ('t307', POINTER(type_307) * 3600),
+    ('t308', POINTER(type_308) * 3600),
+    ('t309', POINTER(type_309) * 3600),
+]
+
+def loadsdata(filePrefix):
+    mk4p = mk4_sdata()
+    mk4io.read_mk4sdata(c_char_p(filePrefix), byref(mk4p))
+    return mk4p
+
+class m4view(object):
+    def __init__(mk4p):
+        self.mk4p = mk4p
